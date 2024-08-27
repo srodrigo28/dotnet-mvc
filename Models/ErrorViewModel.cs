4e143d00127da4c92aa0d877d763033d593ff97c
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-08-27 18:54:20 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace twtodos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+dotnet new mvc --no-https
+
+#### Ref.
+https://www.youtube.com/watch?v=aYgAb5O_QXg
